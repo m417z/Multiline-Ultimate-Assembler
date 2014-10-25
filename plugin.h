8c9f881e9e0f655094c5aadd6a0e78591b251f37
RaMMicHaeL <leahcimmar@gmail.com>
2014-10-25 17:56:00 +0300

    Version bump

--- plugin.h
+++ plugin.h
@@ -12,11 +12,7 @@
 #endif
 
 #define DEF_PLUGINNAME        _T("Multiline Ultimate Assembler")
-#if defined(TARGET_X64DBG)
-#define DEF_VERSION           _T("2.3 (alpha)")
-#else
-#define DEF_VERSION           _T("2.3")
-#endif
+#define DEF_VERSION           _T("2.3.1")
 #define DEF_COPYRIGHT         _T("Copyright (C) 2009-2014 RaMMicHaeL")
 
 #define DECODE_UNKNOWN        0
